6921d4fad4cddad7eb2d30023213822b0e14beee
Jonas Almlöf <>
2020-11-25 11:11:33 +0100

    Bugfixes

--- src/Exon_coverage.py
+++ src/Exon_coverage.py
@@ -31,7 +31,7 @@ for line in bedfile :
         coverage_nr_pos += 1
     depthfile.close()
     avg_coverage = coverage_sum / float(coverage_nr_pos)
-    outfile_all.write(sample + "\t" + exon + "\t" + str(round(avg_coverage),1) + "\n")
+    outfile_all.write(sample + "\t" + exon + "\t" + str(round(avg_coverage,1)) + "\n")
     if avg_coverage < 500 :
-        outfile_low.write(sample + "\t" + exon + "\t" + str(round(avg_coverage),1) + "\n")
+        outfile_low.write(sample + "\t" + exon + "\t" + str(round(avg_coverage,1)) + "\n")
 outfile_all.close()
