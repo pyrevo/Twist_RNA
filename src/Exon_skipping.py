a37626221e0a1aae5bd36af6fc8d44205841a9bd
Jonas Almlöf <>
2021-01-21 12:13:51 +0100

    Bugfix

--- src/Exon_skipping.py
+++ src/Exon_skipping.py
@@ -58,10 +58,10 @@ for line in junction_file:
 result_file.write("Gene\tstart_exon\tend_exon\tsupporting_reads\treads_supporting_normal_splicing\n")
 for unnormal_key in unnormal_junction :
     gene = pos_dict[unnormal_key]
-    nr_unnormal_reads = unnormal_junction[unnormal_key]
+    nr_unnormal_reads = unnormal_junction[unnormal_key][0]
     nr_normal_reads = 0
     if unnormal_key in normal_junction :
-        nr_normal_reads = normal_junction[unnormal_key]
+        nr_normal_reads = normal_junction[unnormal_key][0]
     i_start = unnormal_junction[unnormal_key][1]
     i_end = unnormal_junction[unnormal_key][2]
     if i_start != 100 :
