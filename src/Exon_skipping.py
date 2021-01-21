853056add07c24852addd74eabd3e1f5080c7b98
Jonas Almlöf <>
2021-01-21 08:27:45 +0100

    bedfilefix

--- src/Exon_skipping.py
+++ src/Exon_skipping.py
@@ -6,18 +6,27 @@ junction_file = open(sys.argv[2])
 result_file = open(sys.argv[3], "w")
 
 gene_dict = {}
+pos_dict = {}
 for line in bed_file :
     lline = line.strip().split("\t")
     chrom = lline[0]
     start_pos = int(lline[1])
     end_pos = int(lline[2])
+    key = chrom + "_" + start_pos
     region = lline[3]
     gene = region.split("_")[0]
     exon = region.split("_exon_")[1]
     if gene not in gene_dict :
         gene_dict[gene] = []
     gene_dict[gene].append([chrom, start_pos, end_pos, exon])
+    pos_dict[key] = ""
 
 for line in junction_file:
     lline = line.strip().split("\t")
-    
+    chrom = lline[0]
+    start_pos = lline[1]
+    end_pos = lline[2]
+    nr_reads = int(lline[6])
+    key = "chr" + chrom + "_" + start_pos
+    if key not in pos_dict :
+        continue
